12a4891f6270210f5bbfb7e4e6a586d02885594a
Paul van Woensel <pvanwoensel@gmail.com>
2024-07-25 10:14:04 -0500

    adding small conic gradient chaser

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,11 @@ function App() {
     <main className='flex flex-col items-center justify-center'>
       <Header />
       <div className='w-7/12 max-w-4xl'>
-        <img src="https://utfs.io/f/f9d84dfa-05bb-4929-b3c0-46131686aabf-1x9cqp.jpeg"
-          alt="Paul in Germany" className='image'
-        />
+        <div className='image-container relative'>
+          <img src="https://utfs.io/f/f9d84dfa-05bb-4929-b3c0-46131686aabf-1x9cqp.jpeg"
+            alt="Paul in Germany" className='image'
+          />
+        </div>
       </div>
       <About />
       <div className='flex flex-row justify-start w-7/12 max-w-4xl py-6 gap-6'>
@@ -21,7 +23,7 @@ function App() {
         />
         <div>
           <LinkSection title="Personal projects (more coming soon)" links={[
-            { url: 'https://memory-pi-roan.vercel.app', name: 'Memory Gane' },
+            { url: 'https://memory-pi-roan.vercel.app', name: 'Memory Game (Vue)' },
           ]} />
         </div>
       </div>
